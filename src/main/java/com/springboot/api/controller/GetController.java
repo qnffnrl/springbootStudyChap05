498d4ae341c170cc63e0f3c998c5ffb46075225b
riskerGoldClass <roakfwk30@naver.com>
2022-09-18 19:47:36 +0800

    add : Swagger, Logback 추가

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -1,19 +1,30 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
+
+
 
 @RestController
 @RequestMapping("/api/v1/get-api") // -> 클래스 레벨에서 선언되면 내부 메소드의 URL 앞에 공통 적용됨
 public class GetController {
 
     /**
+     * Logger 객체 정의
+     */
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
+    /**
      * @RequestMapping http://localhost:8080/api/v1/api-get/hello
      */
     @RequestMapping(value = "/hello", method = RequestMethod.GET) // @RequestMapping -> 스프링 4.3 버전 이후로 사용안함
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
@@ -23,15 +34,17 @@ public class GetController {
      */
     @GetMapping("/name")
     public String getName() {
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Flature";
     }
 
     /**
      * @PathVariable을 활용한 GET 메서드 구현
-     * http://locahost:8080/api/v1/api-get/variable1
+     * http://localhost:8080/api/v1/api-get/variable1
      */
     @GetMapping("/variable1/{variable}") //--------------------> 변수명 통일
     public String getVariable(@PathVariable String variable) { //--> 변수명 통일
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
         return variable;
     }
 
@@ -48,11 +61,27 @@ public class GetController {
      * @RequestParam을 이용한 GET 메서드 구현
      * http://localhost:8080/api/vi/get-api/request1?name=value1&email=value2&organization=value3
      */
-    @GetMapping("/request1")
+    @GetMapping("/request")
     public String getRequestParam(
             @RequestParam String name,
             @RequestParam String email,
             @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+
+    /**
+     * 
+     * Swagger 예제
+     * http://localhost:8080/api/vi/get-api/request1?name=value1&email=value2&organization=value3
+     */
+    @ApiOperation(value = "GET 메서드 예제", notes = "@RequestParam을 활용한 GET Method") //대상 API에 대한 설명 작성
+    @GetMapping("/request1")
+    public String getRequestParam1(
+            
+        // @ApiParam -> 매개변수에 대한 설명/설정을 위한 어노테이션
+        @ApiParam(value = "이름", required = true) @RequestParam String name,
+        @ApiParam(value = "이메일", required = true) @RequestParam String email,
+        @ApiParam(value = "회사", required = true) @RequestParam String organization){
         return name + " " + email + " " + organization;
     }
 
